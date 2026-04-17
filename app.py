823d89bf25fb455e30b53cb8c04bfca050ea3a0a
SL <kuashuetlin@gmail.com>
2026-04-18 01:52:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import joblib
-import tensorflow as tf
 import matplotlib.pyplot as plt
 from collections import deque
 from tensorflow.keras.models import load_model
